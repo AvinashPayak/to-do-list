331c196b54d7cbcfa500a709b0cc6c8a1a61d24a
AvinashPayak <avinashpayak@gmail.com>
2023-04-08 21:56:55 +0530

    To Do List  main component logic complete

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
+    <to-do-list/>
   </div>
 </template>
 
 <script>
-
+import ToDoList from './components/ToDoList.vue'
 export default {
   name: 'App',
   components: {
+    ToDoList
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  background-color: #8a2be2;
+  display: flex;
+  justify-content: center;
+}
+
+.btn {
+  @apply rounded-lg px-4 py-2
+}
+
+.btn-primary {
+  background-color: orange;
+  color: white;
 }
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,64 @@
+<template>
+  <section
+    class="bg-white h-[600px] w-[500px] m-10 rounded-[20px] flex flex-col items-center p-5"
+  >
+    <h1 class="my-5 text-3xl font-bold">To Do List</h1>
+    <div class="bg-gray-100 w-full [400px] flex p-2 rounded-xl">
+      <input class="bg-gray-100 p-1 w-full" type="text" v-model="newItem" />
+      <button class="px-5 text-[#8a2be2]" @click="addItem" @keydown.enter="addItem">Add</button>
+    </div>
+    <ul class="w-full mx-5 my-5 text-lg" v-if="todoList.length">
+      <li
+        v-for="item in todoList"
+        :key="item.id"
+        class="flex justify-between w-full"
+      >
+        <p>{{ item.value }}</p>
+        <button class="text-red-500" @click="deleteItem(item.id)">
+          Delete
+        </button>
+      </li>
+    </ul>
+    <p v-else class="text-red-500 my-5">No List Items to display!</p>
+  </section>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "ToDoList",
+  setup() {
+    const newItem = ref("");
+    const todoList = ref([]);
+
+    const addItem = () => {
+      const id = todoList.value.length + 1;
+      const item = { id, value: newItem.value };
+      todoList.value.push(item);
+      console.log("todoList", todoList);
+    };
+
+    const deleteItem = (id) => {
+      const index = todoList.value.findIndex((item) => item.id === id);
+      todoList.value.splice(index, 1);
+    };
+
+    return {
+      newItem,
+      todoList,
+      addItem,
+      deleteItem,
+    };
+  },
+};
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+input:focus {
+  outline: none;
+}
+</style>
